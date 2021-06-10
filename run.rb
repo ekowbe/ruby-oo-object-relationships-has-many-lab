1ed2dbbe9a4e7dac5f7e9544fd7cb9be7b117d61
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-10 17:09:06 +0000

    finished has many lab

---
+++ run.rb
@@ -0,0 +1,15 @@
+require "pry"
+
+require_relative "lib/author.rb"
+require_relative "lib/artist.rb"
+require_relative "lib/post.rb"
+require_relative "lib/song.rb"
+
+post = Post.new("hi world")
+sophie = Author.new('Sophie')
+
+
+binding.pry
+
+
+      
